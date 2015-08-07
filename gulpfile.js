af9769d549f82c0a002b37103db1a592b1aaf389
housewifehacker <housewifehacker@gmail.com>
2015-08-06 23:38:23 -0500

    ESlint with React and ES6

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify')
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var eslint = require('gulp-eslint');
 
 gulp.task('build', function() {
     return browserify({entries: './app.jsx', extensions: ['.jsx'], debug: true})
@@ -11,7 +12,18 @@ gulp.task('build', function() {
         .pipe(gulp.dest('dist'));
 })
 
-gulp.task('watch', ['build'], function() {
+gulp.task('lint', function() {
+    return gulp.src(['*.jsx'])
+	// useEslintrc directs to config options eslintrc
+        .pipe(eslint({
+            useEslintrc: true
+        }))
+        // eslint.format() outputs the lint results to the console.
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('watch', ['build', 'lint'], function() {
     gulp.watch('*.jsx', ['build']);
 });
 
